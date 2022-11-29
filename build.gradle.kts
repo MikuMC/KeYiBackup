10f64772a7bac5cc08e2b97c472de9c945fa1eb0
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2022-11-29 05:29:56 -0500

    Updated Upstream (Purpur & Dependencies)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("org.quiltmc:quiltflower:1.8.1")
+    decompiler("org.quiltmc:quiltflower:1.9.0")
     paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
 }
 
